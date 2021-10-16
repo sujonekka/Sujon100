ace769b242d78ba5a84ef7a5c7ca0cf13087655f
Sujon Ekka <73075960+Sujon100@users.noreply.github.com>
2021-10-16 14:07:53 +0600

    portfolio myself

---
+++ js/script.js
@@ -0,0 +1,38 @@
+new WOW().init();
+
+$(window).on("load", function() {
+
+    $(".loader .inner").fadeOut(500, function() {
+        $(".loader").fadeOut(750);
+    });
+
+})
+
+$(function () {
+    $.scrollUp({
+      scrollName: 'scrollUp', // Element ID
+      topDistance: '300', // Distance from top before showing element (px)
+      topSpeed: 300, // Speed back to top (ms)
+      animation: 'fade', // Fade, slide, none
+      animationInSpeed: 200, // Animation in speed (ms)
+      animationOutSpeed: 200, // Animation out speed (ms)
+      scrollText: '<i class="ti-arrow-up"></i>', // Text for element
+      activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+    });
+  });
+
+  $(document).ready(function(){
+
+    var typed = new Typed('.type',{
+        // Waits 1000ms after typing "First"
+        strings: ['Responsive Design', 'Flexbox - Grid - Bootstrap - ReactJs','Web Development','PHP - MySql - NodeJs- MongoDb', 'APi - Npm- git'],
+        typeSpeed:80,
+        loop:true,
+        backSpeed: 40,
+        showCursor: false,
+      });
+      
+
+
+
+  });
