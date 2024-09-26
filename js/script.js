ac89f329bce5ac5c3bdcc3c1924b6559563434fe
sujonekka <sujonekka100@gmail.com>
2024-09-26 22:16:56 +0600

    var type updated in js/script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,37 @@
 new WOW().init();
 
-$(window).on("load", function() {
-
-    $(".loader .inner").fadeOut(500, function() {
-        $(".loader").fadeOut(750);
-    });
-
-})
+$(window).on("load", function () {
+  $(".loader .inner").fadeOut(500, function () {
+    $(".loader").fadeOut(750);
+  });
+});
 
 $(function () {
-    $.scrollUp({
-      scrollName: 'scrollUp', // Element ID
-      topDistance: '300', // Distance from top before showing element (px)
-      topSpeed: 300, // Speed back to top (ms)
-      animation: 'fade', // Fade, slide, none
-      animationInSpeed: 200, // Animation in speed (ms)
-      animationOutSpeed: 200, // Animation out speed (ms)
-      scrollText: '<i class="ti-arrow-up"></i>', // Text for element
-      activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-    });
+  $.scrollUp({
+    scrollName: "scrollUp", // Element ID
+    topDistance: "300", // Distance from top before showing element (px)
+    topSpeed: 300, // Speed back to top (ms)
+    animation: "fade", // Fade, slide, none
+    animationInSpeed: 200, // Animation in speed (ms)
+    animationOutSpeed: 200, // Animation out speed (ms)
+    scrollText: '<i class="ti-arrow-up"></i>', // Text for element
+    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
   });
+});
 
-  $(document).ready(function(){
-
-    var typed = new Typed('.type',{
-        // Waits 1000ms after typing "First"
-        strings: ['Responsive Design', 'Flexbox - Grid - Bootstrap - ReactJs','Web Development','PHP - MySql - NodeJs- MongoDb', 'APi - Npm- git'],
-        typeSpeed:80,
-        loop:true,
-        backSpeed: 40,
-        showCursor: false,
-      });
-      
-
-
-
+$(document).ready(function () {
+  var typed = new Typed(".type", {
+    // Waits 1000ms after typing "First"
+    strings: [
+      "Responsive Design",
+      "Flexbox - Grid - Bootstrap -Tailwind - VueJs",
+      "Web Development",
+      "PHP - MySql - Laravel",
+      "APi - Git - Github",
+    ],
+    typeSpeed: 80,
+    loop: true,
+    backSpeed: 40,
+    showCursor: false,
   });
+});
